a4c2d4874664d10a9930657cf5b26c129b208da0
ReleeSquirrel <relee_s@hotmail.com>
2025-11-27 03:53:11 -0500

    Implemented startREPL function in repl.ts

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,24 @@
+import { createInterface } from 'node:readline';
+
 export function cleanInput(input: string): string[] {
   const result = input.toLowerCase().trim().split(" ").filter((word) => word.length > 0);
   return result;
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+  rl.prompt();
+  rl.on("line", (input: string) => {
+    const clean_input = cleanInput(input);
+    if (clean_input.length === 0) {
+      rl.prompt();
+    } else {
+      console.log(`Your command was: ${clean_input[0]}`);
+      rl.prompt();
+    }
+  });
 }
